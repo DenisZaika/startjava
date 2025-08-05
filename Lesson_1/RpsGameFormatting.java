d1ecca49e3150570185597feccf4cf815a891788
DenisZaika <zaikadenis2002@mail.ru>
2025-08-05 13:07:30 +0300

    Добавлено решение задания по форматированию
    - комментарии указаны перед соответствующим кодом,
      уровень вложенности комментариев соответствует уровню кода;
    - переименованы переменные для повышения читабельности name1
      в firstPlayerName, name2 в secondPlayerName, sign1 в firstPlayerSign,
      sign2 в secondPlayerSign;
    - экземпляр класса Random переименован из r в random;
    - отступы кода соответствуют границам блоков относительно первых строк;
    - соблюдены пробелы между арифметическими знаками и скобками () {};
    - отформатирован вывод данных на экран с помощью printf;
    - переменная isEqualName1 переименована в isFirstPlayerWinner.

---
+++ Lesson_1/RpsGameFormatting.java
@@ -0,0 +1,63 @@
+import java.util.Random;
+
+// Игра Камень-Ножницы-Бумага
+public class RpsGameFormatting {
+    public static void main(String[] args) throws InterruptedException {
+        String rock = "R";
+        String scissors = "S";
+        String paper = "P";
+
+        // Ход первого игрока
+        String firstPlayerName = "HELL";
+        Random random = new Random();
+        int firstPlayerPosition = random.nextInt(1, 100);
+        String firstPlayerSign = rock;
+        if (firstPlayerPosition > 66) {
+            firstPlayerSign = paper;
+        } else if (firstPlayerPosition > 33) {
+            firstPlayerSign = scissors;
+        }
+        System.out.printf("Ход %s: %n", firstPlayerName);
+        for (int i = 0; i < 5; i++) {
+            System.out.print(rock + "\r");
+            Thread.sleep(100);
+            System.out.print(scissors + "\r");
+            Thread.sleep(100);
+            System.out.print(paper + "\r");
+            Thread.sleep(100);
+        }
+        System.out.println(firstPlayerSign);
+
+        // Ход второго игрока
+        String secondPlayerName = "WALLE";
+        int secondPlayerPosition = random.nextInt(1, 100);
+        String secondPlayerSign = rock;
+        if (secondPlayerPosition > 66) {
+            secondPlayerSign = paper;
+        } else if (secondPlayerPosition > 33) {
+            secondPlayerSign = scissors;
+        }
+        System.out.printf("Ход %s: %n", secondPlayerName);
+        for (int i = 0; i < 5; i++) {
+            System.out.print(rock + "\r");
+            Thread.sleep(100);
+            System.out.print(scissors + "\r");
+            Thread.sleep(100);
+            System.out.print(paper + "\r");
+            Thread.sleep(100);
+        }
+        System.out.println(secondPlayerSign);
+        if (firstPlayerSign.equals(secondPlayerSign)) {
+                System.out.println("Победила дружба!");
+                return;
+        }
+        boolean isFirstPlayerWinner = firstPlayerSign.equals(rock) && secondPlayerSign.equals(scissors) ||
+                firstPlayerSign.equals(scissors) && secondPlayerSign.equals(paper) ||
+                firstPlayerSign.equals(paper) && secondPlayerSign.equals(rock);
+        String winnerName = secondPlayerName;
+        if (isFirstPlayerWinner) {
+            winnerName = firstPlayerName;
+        }
+        System.out.printf("Победитель - %s", winnerName);
+    }
+}
