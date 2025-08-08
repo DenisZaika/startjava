7d630b2c87b9579467f2a807ed0ae74804080fe4
DenisZaika <zaikadenis2002@mail.ru>
2025-08-08 18:30:42 +0300

    Добавлены свойства и поведение класса Person

--- Lesson_2/Person.java
+++ Lesson_2/Person.java
@@ -1,3 +1,29 @@
 public class Person {
-    
+
+    String gender = "male";
+    String name = "Denis";
+    int height = 172;
+    int weight = 71;
+    int age = 25;
+
+    void move() {
+        System.out.println("Прошел 1 метр");
+    }
+
+    boolean sit() {
+        System.out.println("Человек сел");
+        return true;
+    }
+
+    void run() {
+        System.out.println("Человек пробежал 8 метров");
+    }
+
+    void speak() {
+        System.out.println("Человек разговаривает");
+    }
+
+    void learnJava() {
+        System.out.println("Учит Java");
+    }
 }
