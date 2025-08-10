e541ddc6f4083f9a05473a463eec1a281dfb999d
DenisZaika <zaikadenis2002@mail.ru>
2025-08-10 21:27:33 +0300

    Создание объекта класса Wolf
    - полям объекта wolfOne присвоены соответствующие значения и отображаются
      в консоль;
    - вызваны методы объекта.

---
+++ Lesson_2/WolfTest.java
@@ -0,0 +1,21 @@
+public class WolfTest {
+
+    public static void main(String[] args) {
+        Wolf wolfOne = new Wolf();
+        wolfOne.gender = "Самец";
+        wolfOne.name = "Клык";
+        wolfOne.weight = 30;
+        wolfOne.age = 7;
+        wolfOne.coloring = "Серый";
+        System.out.println("Пол: " + wolfOne.gender);
+        System.out.println("Кличка: " + wolfOne.name);
+        System.out.println("Вес: " + wolfOne.weight + " кг");
+        System.out.println("Возраст " + wolfOne.age + " лет");
+        System.out.println("Окрас: " + wolfOne.coloring);
+        wolfOne.move();
+        wolfOne.sit();
+        wolfOne.run();
+        wolfOne.howl();
+        wolfOne.hunting();
+    }
+}
