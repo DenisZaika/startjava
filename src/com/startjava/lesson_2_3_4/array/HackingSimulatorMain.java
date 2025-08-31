cdf54ddb8af459298f6a04856b476803de4e2f02
DenisZaika <zaikadenis2002@mail.ru>
2025-08-31 23:22:00 +0300

    Модифицирован gitignore, решена задача 2
    - в .gitignore добавлено игнорирование
      /.idea/
      /StartJava.iml
    - решена задача №2 по теме массивы в соответствии
      с условием задачи и рекомендаций

---
+++ src/com/startjava/lesson_2_3_4/array/HackingSimulatorMain.java
@@ -0,0 +1,26 @@
+package com.startjava.lesson_2_3_4.array;
+
+public class HackingSimulatorMain {
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+
+    public static void main(String[] args) throws InterruptedException {
+        char[] spinnerSymbols = {'-', '\\', '|', '/'};
+        System.out.print("Hacking: ");
+        for (int i = 0; i < 3; i++) {
+            for (char symbol : spinnerSymbols) {
+                System.out.print(symbol);
+                Thread.sleep(300);
+                System.out.print("\b");
+            }
+        }
+        int selectedNumber = (int) (Math.random() * 100);
+        if (selectedNumber > 70) {
+            System.out.println(ANSI_RED + "Access Granted!" + ANSI_RESET);
+        } else {
+            System.out.println(ANSI_GREEN + "Access Denied!" + ANSI_RESET);
+        }
+    }
+}
