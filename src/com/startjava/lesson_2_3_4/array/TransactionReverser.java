243af6cd37c1694849d5828566e8978ff41f63c7
DenisZaika <zaikadenis2002@mail.ru>
2025-09-02 21:01:03 +0300

    Рефакторинг
    - в main заведены массивы;
    - метод processTransactions() переименован в reverseTransactions() и вместо for используется for-each;
    - метод showResult() переименован в showTransactions();
    - выполнен импорт класса Arrays вместо указания имени пакета java.util

--- src/com/startjava/lesson_2_3_4/array/TransactionReverser.java
+++ src/com/startjava/lesson_2_3_4/array/TransactionReverser.java
@@ -1,28 +1,37 @@
 package com.startjava.lesson_2_3_4.array;
 
-public class TransactionReverserMain {
+import java.util.Arrays;
+
+public class TransactionReverser {
 
     public static void main(String[] args) {
-        showResult(new int[0], processTransactions(new int[0]));
-        showResult(null, processTransactions(null));
-        showResult(new int[]{5}, processTransactions(new int[]{5}));
-        showResult(new int[]{6, 8, 9, 1}, processTransactions(new int[]{6, 8, 9, 1}));
-        showResult(new int[]{13, 8, 5, 3, 2, 1, 1}, processTransactions(new int[]{13, 8, 5, 3, 2, 1, 1}));
+        int[] transactions1 = new int[0];
+        showTransactions(transactions1, reverseTransactions(transactions1));
+        int[] transactions2 = null;
+        showTransactions(transactions2, reverseTransactions(transactions2));
+        int[] transactions3 = new int[]{5};
+        showTransactions(transactions3, reverseTransactions(transactions3));
+        int[] transactions4 = new int[]{6, 8, 9, 1};
+        showTransactions(transactions4, reverseTransactions(transactions4));
+        int[] transactions5 = new int[]{13, 8, 5, 3, 2, 1, 1};
+        showTransactions(transactions5, reverseTransactions(transactions5));
     }
 
-    private static int[] processTransactions(int[] transactions) {
+    private static int[] reverseTransactions(int[] transactions) {
         if (transactions == null || transactions.length == 0) {
             return transactions;
         }
         int len = transactions.length;
         int[] reversedTransactions = new int[len];
-        for (int i = 0; i < len; i++) {
-            reversedTransactions[i] = transactions[len - i - 1];
+        int i = 0;
+        for (int transaction : transactions) {
+            reversedTransactions[len - i - 1] = transaction;
+            i++;
         }
         return reversedTransactions;
     }
 
-    private static void showResult(int[] transactions, int[] reversedTransactions) {
+    private static void showTransactions(int[] transactions, int[] reversedTransactions) {
         if (transactions == null) {
             System.out.println("Ошибка в данных");
             return;
@@ -31,7 +40,7 @@ public class TransactionReverserMain {
             System.out.println("Нет данных\n");
             return;
         }
-        System.out.print("\nИсходные транзакции: " + java.util.Arrays.toString(transactions));
-        System.out.println("\n В обратном порядке: " + java.util.Arrays.toString(reversedTransactions));
+        System.out.print("\nИсходные транзакции: " + Arrays.toString(transactions));
+        System.out.println("\n В обратном порядке: " + Arrays.toString(reversedTransactions));
     }
 }
