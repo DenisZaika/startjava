877ea159a126f2ad490eccfa14de1b4b516f56b3
DenisZaika <zaikadenis2002@mail.ru>
2025-09-12 22:23:42 +0300

    Рефакторинг кода
    - метод perform() переименовал в showSpinner();
    - переменная spinnerSymbols переименована в spins;
    - в методе showSpinner() вместо 2 циклов используется один;
    - вывод \b перенесен к выводу спина;
    - удален лишний код в конце метода showSpinner();
    - метод showResult() переименован в showHackingResult();
    - в этом методе теперь с помощью тернарного оператора выбирается результат взлома.

--- src/com/startjava/lesson_2_3_4/array/HackingSimulator.java
+++ src/com/startjava/lesson_2_3_4/array/HackingSimulator.java
@@ -7,31 +7,23 @@ public class HackingSimulator {
     public static final String ANSI_GREEN = "\u001B[32m";
 
     public static void main(String[] args) throws InterruptedException {
-        int selectedNumber = perform();
-        showResult(selectedNumber);
+        int selectedNumber = showSpinner();
+        showHackingResult(selectedNumber);
     }
 
-    private static int perform() throws InterruptedException {
-        char[] spinnerSymbols = {'-', '\\', '|', '/'};
-        System.out.print("Hacking: ");
-        for (int i = 0; i < 3; i++) {
-            for (char symbol : spinnerSymbols) {
-                System.out.print(symbol);
-                Thread.sleep(300);
-                System.out.print("\b");
-            }
+    private static int showSpinner() throws InterruptedException {
+        char[] spins = {'-', '\\', '|', '/'};
+        System.out.print("Hacking:  ");
+        for (int i = 0; i < 12; i++) {
+            System.out.print("\b" + spins[i % 4]);
+            Thread.sleep(300);
         }
-        System.out.print(spinnerSymbols[0]);
-        Thread.sleep(300);
-        System.out.print("\b");
         return (int) (Math.random() * 100);
     }
 
-    private static void showResult(int selectedNumber) {
-        if (selectedNumber > 70) {
-            System.out.println(ANSI_RED + "Access Granted!" + ANSI_RESET);
-        } else {
-            System.out.println(ANSI_GREEN + "Access Denied!" + ANSI_RESET);
-        }
+    private static void showHackingResult(int selectedNumber) {
+        System.out.print("\b" +
+                (selectedNumber > 70 ? ANSI_RED + " Access Granted!" : "\b" + ANSI_GREEN + " Access Denied!") +
+                ANSI_RESET);
     }
 }
