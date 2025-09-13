6637811ad2862d3d8ced80878ef92659dbeb47f5
DenisZaika <zaikadenis2002@mail.ru>
2025-09-13 13:26:24 +0300

    Рефакторнг кода
    - метод переименован из showSpinner() в hackPassword();
    - ограничение в условии оператора for теперь вычисляется через длину массива spins;
    - в методе showHackingResult() переменнная selectedNumber переименована в accessCode;
    - тернарный оператор отформатирован в несколько строк.

--- src/com/startjava/lesson_2_3_4/array/HackingSimulator.java
+++ src/com/startjava/lesson_2_3_4/array/HackingSimulator.java
@@ -7,23 +7,26 @@ public class HackingSimulator {
     public static final String ANSI_GREEN = "\u001B[32m";
 
     public static void main(String[] args) throws InterruptedException {
-        int selectedNumber = showSpinner();
+        int selectedNumber = hackPassword();
         showHackingResult(selectedNumber);
     }
 
-    private static int showSpinner() throws InterruptedException {
+    private static int hackPassword() throws InterruptedException {
+        int selectedNumber = (int) (Math.random() * 100);
         char[] spins = {'-', '\\', '|', '/'};
         System.out.print("Hacking:  ");
-        for (int i = 0; i < 12; i++) {
+        for (int i = 0; i < 3 * spins.length; i++) {
             System.out.print("\b" + spins[i % 4]);
             Thread.sleep(300);
         }
-        return (int) (Math.random() * 100);
+        return selectedNumber;
     }
 
-    private static void showHackingResult(int selectedNumber) {
+    private static void showHackingResult(int accessCode) {
         System.out.print("\b" +
-                (selectedNumber > 70 ? ANSI_RED + " Access Granted!" : "\b" + ANSI_GREEN + " Access Denied!") +
+                (accessCode > 70
+                        ? ANSI_RED + " Access Granted!"
+                        : ANSI_GREEN + " Access Denied!") +
                 ANSI_RESET);
     }
 }
