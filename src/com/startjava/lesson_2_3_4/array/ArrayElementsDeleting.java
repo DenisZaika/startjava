0e9efa144fe2de840dcfcca95b160fdd67322084
DenisZaika <zaikadenis2002@mail.ru>
2025-09-14 14:50:54 +0300

    Рефакторинг кода
    - теперь исходный и измененный массив выводятся в 2 строки (по 8 и 7 чисел на строку);
    - теперь в случае ошибки исходный массив не выводится;
    - метод deleteElements переименован в deleteElementsMoreThan
    - теперь вместо удаления элементов массива с индексами больше заданного, удаляются из массива те значения, которые > конкретного значения по конкретному индексу;
    - добавлен метод validIndex, который проверяет, является ли значение индекса корректным.

--- src/com/startjava/lesson_2_3_4/array/ArrayElementsDeleting.java
+++ src/com/startjava/lesson_2_3_4/array/ArrayElementsDeleting.java
@@ -7,9 +7,10 @@ public class ArrayElementsDeleting {
         float[] array = new float[15];
         for (int index : indexes) {
             fillArray(array);
-            System.out.print("\nИсходный массив: ");
-            showArray(array);
-            if (deleteElements(index, array)) {
+            if (validateIndex(index, array)) {
+                System.out.print("\nИсходный массив: ");
+                showArray(array);
+                deleteElementsMoreThan(index, array);
                 System.out.print("Измененный массив: ");
                 showArray(array);
             }
@@ -22,22 +23,30 @@ public class ArrayElementsDeleting {
         }
     }
 
-    private static boolean deleteElements(int num, float[] array) {
-        if (num < 0 || num >= array.length) {
-            System.out.println("Ошибка: введен некорректный индекс " + num);
+    private static boolean validateIndex(int index, float[] array) {
+        if (index < 0 || index >= array.length) {
+            System.out.println("\nОшибка: введен некорректный индекс " + index);
             System.out.println("Допустимые для ввода индексы от 0 до " + (array.length - 1));
             return false;
-        }
-        System.out.printf("array[%d] = %.3f%n", num, array[num]);
-        for (int i = num + 1; i < array.length; i++) {
-            array[i] = 0;
         }
         return true;
     }
 
+    private static void deleteElementsMoreThan(int index, float[] array) {
+        System.out.printf("array[%d] = %.3f%n", index, array[index]);
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > array[index]) {
+                array[i] = 0;
+            }
+        }
+    }
+
     private static void showArray(float[] array) {
-        for (float num : array) {
-            System.out.printf("%.3f ", num);
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf("%.3f ", array[i]);
+            if ((i + 1) % 8 == 0) {
+                System.out.println();
+            }
         }
         System.out.println();
     }
