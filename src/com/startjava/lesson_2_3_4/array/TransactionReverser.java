950a5103b3017957e618b38350c73eb8530039c4
DenisZaika <zaikadenis2002@mail.ru>
2025-09-14 12:18:12 +0300

    Форматирование отступов массива в main()

--- src/com/startjava/lesson_2_3_4/array/TransactionReverser.java
+++ src/com/startjava/lesson_2_3_4/array/TransactionReverser.java
@@ -6,11 +6,11 @@ public class TransactionReverser {
 
     public static void main(String[] args) {
         int[][] originTransactions = {
-                {},
-                null,
-                {5},
-                {6, 8, 9, 1},
-                {13, 8, 5, 3, 2, 1, 1}};
+            {},
+            null,
+            {5},
+            {6, 8, 9, 1},
+            {13, 8, 5, 3, 2, 1, 1}};
         for (int[] transactions : originTransactions) {
             showTransactions(transactions, reverseTransactions(transactions));
         }
