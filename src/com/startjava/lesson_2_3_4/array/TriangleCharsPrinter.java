7a8e59a317eaf44d43cc35b0706fe5f4c39b702e
DenisZaika <zaikadenis2002@mail.ru>
2025-09-19 21:12:23 +0300

    Рефакторинг кода
    - все слова pyramid заменены на triangle;
    - метод sortChars переименован в formTriangleParts;
    - метод printCharsPyramid переименован в printTriangleChars;
    - переменная charRepeatsCount переименована в charCount;
    - в цикле for метода formTriangleParts вместо rangeStart + i и rangeEnd - i сделаны инкремент и декремент.

--- src/com/startjava/lesson_2_3_4/array/TriangleCharsPrinter.java
+++ src/com/startjava/lesson_2_3_4/array/TriangleCharsPrinter.java
@@ -3,40 +3,38 @@ package com.startjava.lesson_2_3_4.array;
 public class TriangleCharsPrinter {
 
     public static void main(String[] args) {
-        printCharsPyramid(sortChars('0', '9', true));
-        printCharsPyramid(sortChars('/', '!', false));
-        printCharsPyramid(sortChars('A', 'J', false));
+        printTriangleChars(formTriangleParts('0', '9', true));
+        printTriangleChars(formTriangleParts('/', '!', false));
+        printTriangleChars(formTriangleParts('A', 'J', false));
     }
 
-    private static String[] sortChars(char rangeStart, char rangeEnd, boolean isAscendingOrder) {
+    private static String[] formTriangleParts(char rangeStart, char rangeEnd, boolean isAscendingOrder) {
         if (rangeStart > rangeEnd) {
             System.out.printf("Ошибка: левая граница (%d) > правой (%d)%n%n", (int) rangeStart, (int) rangeEnd);
             return new String[0];
         }
-        String[] pyramidParts = new String[rangeEnd - rangeStart + 1];
-        int charRepeatsCount = 1;
-        for (char i = 0; i < pyramidParts.length; i++) {
-            char currSymbol = isAscendingOrder ?
-                    (char) (rangeStart + i) :
-                    (char) (rangeEnd - i);
-            pyramidParts[i] = String.valueOf(currSymbol).repeat(charRepeatsCount);
-            charRepeatsCount += 2;
+        String[] triangleParts = new String[rangeEnd - rangeStart + 1];
+        int charCount = 1;
+        for (char i = 0; i < triangleParts.length; i++) {
+            char currSymbol = isAscendingOrder ? rangeStart++ : rangeEnd--;
+            triangleParts[i] = String.valueOf(currSymbol).repeat(charCount);
+            charCount += 2;
         }
-        return pyramidParts;
+        return triangleParts;
     }
 
-    private static void printCharsPyramid(String[] pyramidParts) {
-        int len = pyramidParts.length;
+    private static void printTriangleChars(String[] triangleParts) {
+        int len = triangleParts.length;
         if (len == 0) {
             return;
         }
-        StringBuilder charsPyramid = new StringBuilder();
+        StringBuilder charsTriangle = new StringBuilder();
         for (int i = 0; i < len; i++) {
             int spaceCount = len - (i + 1);
-            charsPyramid.append(" ".repeat(spaceCount))
-                        .append(pyramidParts[i])
+            charsTriangle.append(" ".repeat(spaceCount))
+                        .append(triangleParts[i])
                         .append("\n");
         }
-        System.out.println(charsPyramid);
+        System.out.println(charsTriangle);
     }
 }
