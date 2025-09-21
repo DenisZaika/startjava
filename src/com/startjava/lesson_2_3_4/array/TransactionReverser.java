2e6c666c66d433d205729a55e7d142e972568d45
DenisZaika <zaikadenis2002@mail.ru>
2025-09-21 10:28:07 +0300

    Рефакторинг кода
    - удалена пустая строка после объявления класса;
    - отступы передем массивами (инициализации массива originTransactions) равны 8 вместо 4.

--- src/com/startjava/lesson_2_3_4/array/TransactionReverser.java
+++ src/com/startjava/lesson_2_3_4/array/TransactionReverser.java
@@ -3,14 +3,13 @@ package com.startjava.lesson_2_3_4.array;
 import java.util.Arrays;
 
 public class TransactionReverser {
-
     public static void main(String[] args) {
         int[][] originTransactions = {
-            {},
-            null,
-            {5},
-            {6, 8, 9, 1},
-            {13, 8, 5, 3, 2, 1, 1}};
+                {},
+                null,
+                {5},
+                {6, 8, 9, 1},
+                {13, 8, 5, 3, 2, 1, 1}};
         for (int[] transactions : originTransactions) {
             showTransactions(transactions, reverseTransactions(transactions));
         }
