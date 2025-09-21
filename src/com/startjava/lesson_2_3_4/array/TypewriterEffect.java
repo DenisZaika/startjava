ffbd5f9b9a1b8bf44bd37cde9ea7a0e93c050538
DenisZaika <zaikadenis2002@mail.ru>
2025-09-21 13:08:49 +0300

    Рефакторинг кода
    - в main входные данные помещены в массив texts, убраны текстовые блоки, элементы texts обрабатываются в цикле;
    - метод searchShortestLongestWord() переименован в findShortestAndLongestWordIndexes();
    - в этом методе split переносен на исходную строку;
    - длина слова берется по ходу в цикле без создания отдельного массива wordsLens;
    - проверка индексов переписана в сортировку sort() и перенесена в метод toUpperCaseRange, который использует индексы;
    - метод formatText() переименован в toUpperCaseRange;
    - форматирование слов переписано через split по пробелам и upperCase в цикле;
    - метод printFormattedText() переименован в Type().

--- src/com/startjava/lesson_2_3_4/array/TypewriterEffect.java
+++ src/com/startjava/lesson_2_3_4/array/TypewriterEffect.java
@@ -1,31 +1,23 @@
 package com.startjava.lesson_2_3_4.array;
 
+import java.util.Arrays;
+
 public class TypewriterEffect {
     public static void main(String[] args) throws InterruptedException {
-        String text1 = """
-                Чтобы написать чистый код, мы сначала пишем грязный код, затем рефакторим его.
-                - Robert Martin
-                """;
-        int[] minMaxIndexes = searchShortestLongestWord(text1);
-        printFormattedText(formatText(minMaxIndexes, text1));
-
-        String text2 = """
-                Java - это C++, из которого убрали все пистолеты, ножи и дубинки.
-                - James Gosling
-                """;
-        minMaxIndexes = searchShortestLongestWord(text2);
-        printFormattedText(formatText(minMaxIndexes, text2));
-
+        String text1 = "Чтобы написать чистый код, мы сначала пишем грязный код, затем рефакторим его.\n" +
+                "- Robert Martin";
+        String text2 = "Java - это C++, из которого убрали все пистолеты, ножи и дубинки.\n" +
+                "- James Gosling";
         String text3 = null;
-        minMaxIndexes = searchShortestLongestWord(text3);
-        printFormattedText(formatText(minMaxIndexes, text3));
-
         String text4 = "";
-        minMaxIndexes = searchShortestLongestWord(text4);
-        printFormattedText(formatText(minMaxIndexes, text4));
+        String[] texts = {text1, text2, text3, text4};
+        for (String text : texts) {
+            int[] minMaxIndexes = findShortestAndLongestWordIndexes(text);
+            type(toUpperCaseRange(minMaxIndexes, text));
+        }
     }
 
-    private static int[] searchShortestLongestWord(String originText) {
+    private static int[] findShortestAndLongestWordIndexes(String originText) {
         if (originText == null) {
             System.out.println("Ошибка в данных");
             return new int[0];
@@ -35,52 +27,42 @@ public class TypewriterEffect {
             return new int[0];
         }
 
-        String[] words = originText.replaceAll("\\p{P}", "")
-                                     .split("\\s+");
-        int[] wordsLens = new int[words.length];
-        for (int i = 0; i < wordsLens.length; i++) {
-            wordsLens[i] = words[i].length();
-        }
+        String[] words = originText.replaceAll("\\p{P}", "").split("\\s+");
         int maxLengthIndex = 0;
         int minLengthIndex = 0;
-        for (int i = 0; i < wordsLens.length; i++) {
-            if (wordsLens[i] > wordsLens[maxLengthIndex]) {
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].length() > words[maxLengthIndex].length()) {
                 maxLengthIndex = i;
             }
-            if (wordsLens[i] < wordsLens[minLengthIndex]) {
+            if (words[i].length() < words[minLengthIndex].length()) {
                 minLengthIndex = i;
             }
         }
-        return minLengthIndex > maxLengthIndex
-                ? new int[]{maxLengthIndex, minLengthIndex}
-                : new int[] {minLengthIndex, maxLengthIndex};
+        return new int[]{minLengthIndex, maxLengthIndex};
     }
 
-    private static String formatText(int[] minMaxIndexes, String originText) {
+    private static String toUpperCaseRange(int[] minMaxIndexes, String originText) {
         if (minMaxIndexes.length == 0) {
             return "";
         }
-        StringBuilder formattedText = new StringBuilder();
-        String[] lines = originText.split("\n");
+        Arrays.sort(minMaxIndexes);
+        String textWithMarkers = originText.replace("\n", "/n");
+        String[] words = textWithMarkers.split("\\s+");
         int wordIndex = 0;
-        for (String line : lines) {
-            String[] wordsInLine = line.split("\\s+");
-            for (String word : wordsInLine) {
-                if (word.matches("\\p{P}")) {
-                    formattedText.append(word).append(" ");
-                    continue;
+        for (int i = 0; i < words.length; i++) {
+            if (!words[i].matches("\\p{P}")) {
+                if ((wordIndex >= minMaxIndexes[0]) && (wordIndex <= minMaxIndexes[1])) {
+                    words[i] = words[i].toUpperCase();
                 }
-                formattedText.append(((wordIndex >= minMaxIndexes[0]) && (wordIndex <= minMaxIndexes[1]))
-                        ? word.toUpperCase() : word);
-                formattedText.append(" ");
                 wordIndex++;
             }
-            formattedText.append("\n");
         }
-        return String.valueOf(formattedText);
+        String formattedText = String.join(" ", words);
+        formattedText = formattedText.replaceAll("(?i)/n", "\n");
+        return formattedText;
     }
 
-    private static void printFormattedText(String originText) throws InterruptedException {
+    private static void type(String originText) throws InterruptedException {
         if (originText.isEmpty()) {
             return;
         }
@@ -88,5 +70,6 @@ public class TypewriterEffect {
             System.out.print(letter);
             Thread.sleep(100);
         }
+        System.out.println();
     }
 }
