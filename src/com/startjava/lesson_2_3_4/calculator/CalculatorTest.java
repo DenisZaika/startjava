5ab88a6893ab8b8aca6f3c0e6bbc569a5469cdc0
DenisZaika <zaikadenis2002@mail.ru>
2025-10-05 19:01:04 +0300

    Модификация класса
    - вложенный цикл переписан if-else
    - ввод пользователя преобразовывается к нижнему регистру
    - выражение теперь вводится в строку и помещается в массив mathExp
    - при вводе пользователем ни yes и ни no выводите альтернативный текст
    - вывод числа-результата через DecimalFormat с возможным сохранением до
      3 чисел после запятой

--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -1,50 +1,41 @@
 package com.startjava.lesson_2_3_4.calculator;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class CalculatorTest {
-
     public static void main(String[] args) {
         String userAnswer = "yes";
         Scanner scan = new Scanner(System.in);
 
-        while ("yes".equals(userAnswer)) {
-            Calculator calc = new Calculator();
-
-            System.out.print("\nВведите первое число: ");
-            int operand1 = scan.nextInt();
-            calc.setOperand1(operand1);
-            char sign;
-
-            while (true) {
-                System.out.print("Введите знак операции (+, -, *, /, ^, %): ");
-                sign = scan.next().charAt(0);
-                if (calc.setSign(sign)) {
-                    break;
+        while (!"no".equals(userAnswer)) {
+            if ("yes".equals(userAnswer)) {
+                System.out.print("\nВведите выражение из трех аргументов, например, 2 ^ 10: ");
+                String[] mathExp = scan.nextLine().split("\\s+");
+                if (!mathExp[1].equals("+") && !mathExp[1].equals("-") && !mathExp[1].equals("*") &&
+                        !mathExp[1].equals("/") && !mathExp[1].equals("^") && !mathExp[1].equals("%")) {
+                    System.out.println("Ошибка: операция (" + mathExp[1] + ") не поддерживается");
+                    continue;
                 }
-                System.out.printf("Ошибка: операция '%c' не поддерживается%n", sign);
+                Calculator calc = new Calculator(mathExp);
+                double result = calc.calculate();
+                outputResult(mathExp, result);
+                System.out.print("Хотите продолжить вычисления? [yes/no]: ");
+            } else {
+                System.out.print("Введите корректный ответ [yes / no]: ");
             }
-
-            int operand2;
-            System.out.print("Введите второе число: ");
-            operand2 = scan.nextInt();
-            calc.setOperand2(operand2);
-
-            double result = calc.calculate();
-            outputResult(operand1, sign, operand2, result);
-
-            do {
-                System.out.print("Хотите продолжить вычисления? [yes/no]:");
-                userAnswer = scan.next();
-            } while (!"yes".equals(userAnswer) && !"no".equals(userAnswer));
-        } 
+            userAnswer = scan.nextLine().toLowerCase();
+        }
     }
 
-    private static void outputResult(int operand1, char sign, int operand2, double result) {
+    private static void outputResult(String[] mathExp, double result) {
         if (Double.isNaN(result)) {
             System.out.println("Ошибка: деление на ноль запрещено");
         } else {
-            System.out.printf("%d %c %d = %.3f%n", operand1, sign, operand2, result);
+            String pattern = "#.###";
+            DecimalFormat df = new DecimalFormat(pattern);
+            String formattedResult = df.format(result).replace('.', ',');
+            System.out.println(mathExp[0] + " " + mathExp[1] + " " + mathExp[2] + " = " + formattedResult);
         }
     }
 }
