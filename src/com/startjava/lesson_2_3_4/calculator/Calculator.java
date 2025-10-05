adb29598460d0106864c149b9b6c0d726e433537
DenisZaika <zaikadenis2002@mail.ru>
2025-10-05 19:08:04 +0300

    Модификация класса
    - в конструкторе делим на части массив мат выражения
    - Integer.parseInt() используется для преобразования строки в число
    - заменена реализация ^ и % на методы из Math
    - удалены сеттеры

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,50 +1,28 @@
 package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
+    private final int operand1;
+    private final char sign;
+    private final int operand2;
 
-    private int operand1;
-    private char sign;
-    private int operand2;
-
-    public void setOperand1(int operand1) {
-        this.operand1 = operand1;
-    }
-
-    public boolean setSign(char sign) {
-        if (sign != '+' && sign != '-' && sign != '*' && sign != '/' && sign != '^' && sign != '%') {
-            return false;
-        }
-        this.sign = sign;
-        return true;
-    }
-
-    public void setOperand2(int operand2) {
-        this.operand2 = operand2;
+    public Calculator(String[] mathExp) {
+        operand1 = Integer.parseInt(mathExp[0]);
+        sign = mathExp[1].charAt(0);
+        operand2 = Integer.parseInt(mathExp[2]);
     }
 
     public double calculate() {
         if (operand2 == 0 && (sign == '/' || sign == '%')) {
             return Double.NaN;
         }
-        switch (sign) {
-            case '+':
-                return operand1 + operand2;
-            case '-':
-                return operand1 - operand2;
-            case '*':
-                return operand1 * operand2;
-            case '/':
-                return (double) operand1 / operand2;
-            case '^':
-                int result = 1;
-                for (int i = 1; i <= Math.abs(operand2); i++) {
-                    result *= operand1;
-                }
-                return operand2 > 0 ? result : (double) 1 / result;
-            case '%':
-                return operand1 % operand2;
-            default:
-                return 0;
-        }
+        return switch (sign) {
+            case '+' -> operand1 + operand2;
+            case '-' -> operand1 - operand2;
+            case '*' -> operand1 * operand2;
+            case '/' -> (double) operand1 / operand2;
+            case '^' -> Math.pow(operand1, operand2);
+            case '%' -> Math.floorMod(operand1, operand2);
+            default -> 0;
+        };
     }
 }
