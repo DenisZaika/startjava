0cd92a8be21fc05d2c5c5f24cae5e196fc7c5eed
DenisZaika <zaikadenis2002@mail.ru>
2025-10-10 19:35:06 +0300

    Модификация класса
    - добавлены константы диапазона выбираемого числа
    - неизменяемые поля теперь final
    - в start добавлено отображение числа попыток игроков
    - добавлена переменная gameWon статус игры, для выхода
      из цикла при победе одного из игроков
    - выход из цикла возможен при отстуствии попыток у каждого игрока
    - при окончании игры выводятся все названные числа игроков методом
      printPlayerNumbers
    - в методе processPlayerMove обрабатывается и инициируется  ввод числа игроком
    - в методе inputNumber реализована проверка ввода числа через кастомное
      исключение InvalidNumberException
    - isCorrectGuess метод обрабатывает случай угадывания игроком числа
    - handleIncorrectGuess обрабатывает некорректное угадывание
    - giveHint выводит подсказку в случае ошибки угадывания
    - resetGame сбрасывает названные числа игроков и их попытки

--- src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
@@ -1,11 +1,15 @@
 package com.startjava.lesson_2_3_4.guess;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class GuessNumber {
+    private static final int RANGE_MIN = 1;
+    private static final int RANGE_MAX = 100;
 
-    private Player player1;
-    private Player player2;
+    private final Player player1;
+    private final Player player2;
+    private int secretNumber;
 
     public GuessNumber(Player player1, Player player2) {
         this.player1 = player1;
@@ -13,28 +17,81 @@ public class GuessNumber {
     }
 
     public void start() {
-        Scanner scan = new Scanner(System.in);
-        int secretNumber = (int) (Math.random() * 100 + 1);
+        secretNumber = (int) (Math.random() * RANGE_MAX + RANGE_MIN);
+        System.out.println("\nИгра началась! У каждого игрока по " + Player.MAX_ATTEMPTS + " попыток");
         Player currPlayer = player1;
+        boolean gameWon = false;
         do {
-            inputNumber(scan, currPlayer);
-            if (currPlayer.getNumber() == secretNumber) {
-                System.out.printf("Игрок %s победил!%n", currPlayer.getName());
-                return;
+            currPlayer.incrementAttempts();
+            if (isCorrectGuess(processPlayerMove(currPlayer), currPlayer)) {
+                gameWon = true;
+            } else {
+                handleIncorrectGuess(currPlayer);
+                currPlayer = currPlayer == player1 ? player2 : player1;
             }
-            String hint = currPlayer.getNumber() < secretNumber ? "меньше" : "больше";
-            System.out.printf("%d %s того, что загадал компьютер%n", currPlayer.getNumber(), hint);
-            System.out.printf("Игрок %s не угадал число. Ход переходит к следующему игроку%n",
-                    currPlayer.getName());
-            currPlayer = currPlayer == player1 ? player2 : player1;
+        } while ((player1.hasAttempts() || player2.hasAttempts()) && !gameWon);
+        if (!gameWon) {
+            System.out.println("\nНикто не угадал число!");
+        }
+        System.out.println("\nЗагаданное число: " + secretNumber);
+        System.out.println("Введенные числа игроков: ");
+        printPlayerNumbers(player1);
+        printPlayerNumbers(player2);
+        resetGame();
+    }
+
+    private int processPlayerMove(Player player) {
+        System.out.println("\nПопытка " + player.getAttemptNumber());
+        System.out.printf("Число вводит %s: ", player.getName());
+        return inputNumber(player);
+    }
+
+    private int inputNumber(Player player) {
+        Scanner scan = new Scanner(System.in);
+        int playerGuess;
+        do {
+            try {
+                playerGuess = scan.nextInt();
+                player.setNumbers(playerGuess);
+                return playerGuess;
+            } catch (InvalidNumberException e) {
+                System.out.print(e.getMessage());
+            }
         } while (true);
     }
 
-    private void inputNumber(Scanner scan, Player player) {
-        int playerGuess;
-        do {
-            System.out.printf("%s угадывает число: ", player.getName());
-            playerGuess = scan.nextInt();
-        } while (!player.setNumber(playerGuess));
+    private boolean isCorrectGuess(int playerGuess, Player player) {
+        if (playerGuess == secretNumber) {
+            System.out.printf("%s угадал число %d с %d-й попытки%n", player.getName(), playerGuess,
+                    player.getAttemptNumber());
+            return true;
+        }
+        return false;
+    }
+
+    private void handleIncorrectGuess(Player player) {
+        giveHint(player.getNumbers()[player.getAttemptNumber() - 1]);
+        System.out.printf("Игрок %s не угадал число. Ход переходит к следующему игроку%n",
+                player.getName());
+        if (player.getAttemptNumber() == Player.MAX_ATTEMPTS) {
+            System.out.println("У " + player.getName() + " закончились попытки");
+        }
+    }
+
+    private void giveHint(int playerGuess) {
+        String hint = playerGuess < secretNumber ? "меньше" : "больше";
+        System.out.printf("%d %s того, что загадал компьютер%n", playerGuess, hint);
+    }
+
+    private void printPlayerNumbers(Player player) {
+        System.out.println(player.getName() + ": " +
+                Arrays.toString(player.getNumbers()).replaceAll("[\\[\\],]", ""));
+    }
+
+    private void resetGame() {
+        player1.cleanNumbers();
+        player1.resetAttempts();
+        player2.cleanNumbers();
+        player2.resetAttempts();
     }
 }
