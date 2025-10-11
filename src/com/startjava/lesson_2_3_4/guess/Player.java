ee39739b672da3902aaf3c0537fd59cbcb8cc554
DenisZaika <zaikadenis2002@mail.ru>
2025-10-11 21:45:06 +0300

    Модификация класса
    - импортирован класс Arrays
    - создано поле - статическая константа - MAX_ATTEMPTS, хранящая
      макс. число попыток
    - добавлено поле AttemptNumber
    - неизменяемы поля сделаны final константами
    - поля инициализируются в конструкторе класса
    - метод getNumber модифицирован в getNumbers, возвращающий с помощью
      Arrays.copyOf названные числа игроком
    - метод setNumber модифицирован в setNumbers, где перед добавлением
      числа в массив названных игроком чисел, происходит его валидация
      с помощью кастомного исключения InvalidNumberException
    - добавлен геттер для поля attemptNumber
    - добавлен метод incrementAttempts для инкремента поля attemptNumber
      если номер попытки меньше макс. их числу
    - добавлен метод hasAttempts который возвращает бул значение наличия
      попыток у игрока
    - resetAttempts метод сбрасывает номер попытки игрока до нуля
    - cleanNumbers метод чистит массив названных игроком чисел

--- src/com/startjava/lesson_2_3_4/guess/Player.java
+++ src/com/startjava/lesson_2_3_4/guess/Player.java
@@ -1,28 +1,54 @@
 package com.startjava.lesson_2_3_4.guess;
 
-public class Player {
+import java.util.Arrays;
 
-    private String name;
-    private int number;
+public class Player {
+    public static final int MAX_ATTEMPTS = 10;
+
+    private final String name;
+    private final int[] numbers;
+    private int attemptNumber;
 
     public Player(String name) {
         this.name = name;
+        numbers = new int[MAX_ATTEMPTS];
+        attemptNumber = 0;
     }
 
     public String getName() {
         return name;
     }
 
-    public int getNumber() {
-        return number;
+    public int[] getNumbers() {
+        return Arrays.copyOf(numbers, attemptNumber);
     }
 
-    public boolean setNumber(int number) {
+    public void setNumbers(int number) throws InvalidNumberException {
         if (number < 1 || number > 100) {
-            System.out.println("Ошибка: число должно быть в диапазоне [1,100]");
-            return false;
+            throw new InvalidNumberException("Число должно входить в отрезок [1, 100]\nПопробуйте еще раз: ");
         }
-        this.number = number;
-        return true;
+        numbers[attemptNumber - 1] = number;
+    }
+
+    public int getAttemptNumber() {
+        return attemptNumber;
+    }
+
+    public void incrementAttempts() {
+        if (attemptNumber < MAX_ATTEMPTS) {
+            attemptNumber++;
+        }
+    }
+
+    public boolean hasAttempts() {
+        return attemptNumber < MAX_ATTEMPTS;
+    }
+
+    public void resetAttempts() {
+        attemptNumber = 0;
+    }
+
+    public void cleanNumbers() {
+        Arrays.fill(numbers, 0, attemptNumber, 0);
     }
 } 
