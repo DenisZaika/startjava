5fec3bef37d2e15b277bcf2eb696329958c2c2d2
DenisZaika <zaikadenis2002@mail.ru>
2025-10-28 22:59:45 +0300

    Рефакторинг кода
    - из main вывод меню вынес в метод printMenu()
    - удалил метод findLongestBookData - теперь используется геттер getLength
      из класса Bookshelf
    - name переименовал в title
    - поменял местами case 1 и case 2
    - сообщения об ошибках/удачных операциях с книгами вынес из методов
      Bookshelf в cases с соответствующей проверкой на null
    - inputBookName переименован в inputTitle

--- src/com/startjava/graduation/BookshelfTest.java
+++ src/com/startjava/graduation/BookshelfTest.java
@@ -4,27 +4,19 @@ import com.startjava.lesson_2_3_4.array.TypewriterEffect;
 import java.util.Scanner;
 
 public class BookshelfTest {
+    private static final Scanner SCAN = new Scanner(System.in);
     private static final int MIN_MENU_POINT = 1;
     private static final int MAX_MENU_POINT = 5;
-    private static final Scanner SCAN = new Scanner(System.in);
 
     public static void main(String[] args) throws InterruptedException {
         String welcomeText = "Добро пожаловать в книжный шкаф!\n" +
                 "Здесь Вы можете найти интересные книги или добавить новые в свою коллекцию!";
         TypewriterEffect.type(welcomeText);
         Bookshelf bookshelf = new Bookshelf();
-        String menu = """
-                1. Найти книгу
-                2. Добавить книгу
-                3. Удалить книгу
-                4. Очистить шкаф
-                5. Закрыть шкаф
-                """;
         while (true) {
             printBookshelf(bookshelf);
-            System.out.println("\nМеню: \n" + menu);
+            printMenu();
             if (!handleMenu(bookshelf)) {
-                System.out.println("Шкаф закрыт.");
                 return;
             }
             do {
@@ -37,7 +29,7 @@ public class BookshelfTest {
         if (bookshelf.getBookCount() >= 1) {
             System.out.println("\nВ шкафу книг - " + bookshelf.getBookCount() +
                     ", свободно полок - " + bookshelf.getFreeShelfCount() + "\n");
-            int longestBookData = findLongestBookData(bookshelf);
+            int longestBookData = bookshelf.getLength();
             for (Book book : bookshelf.getBooks()) {
                 System.out.print("|" + book.toString() +
                         " ".repeat(longestBookData - book.toString().length()) + "|\n");
@@ -49,14 +41,15 @@ public class BookshelfTest {
         }
     }
 
-    private static int findLongestBookData(Bookshelf bookshelf) {
-        int max = 0;
-        for (Book book : bookshelf.getBooks()) {
-            if (book.toString().length() > max) {
-                max = book.toString().length();
-            }
-        }
-        return max;
+    private static void printMenu() {
+        System.out.println("""
+                \nМеню:
+                1. Добавить книгу
+                2. Найти книгу
+                3. Удалить книгу
+                4. Очистить шкаф
+                5. Закрыть шкаф
+                """);
     }
 
     private static boolean handleMenu(Bookshelf bookshelf) {
@@ -67,23 +60,36 @@ public class BookshelfTest {
             isCorrectChoice = true;
             switch (userChoice) {
                 case "1":
-                    System.out.println(bookshelf.find(inputBookName()));
-                    break;
-                case "2":
-                    if (bookshelf.getFreeShelfCount() == 0) {
+                    if (bookshelf.getFreeShelfCount() == 0 || bookshelf.add(addBookToShelf()) == 0) {
                         System.out.println("\nОшибка: книга не может быть сохранена, " +
                                 "в шкафу закончилось место!");
                     } else {
-                        bookshelf.add(addBookToShelf());
+                        System.out.println("\nКнига успешно добавлена!");
+                    }
+                    break;
+                case "2":
+                    Book foundBook = bookshelf.find(inputTitle());
+                    if (foundBook == null) {
+                        System.out.println("\nОшибка: книга не найдена!");
+                    } else {
+                        System.out.println("\nКнига успешно найдена!");
+                        System.out.println("Найденная книга: " + foundBook);
                     }
                     break;
                 case "3":
-                    bookshelf.delete(inputBookName());
+                    Book deletedBook = bookshelf.delete(inputTitle());
+                    if (deletedBook == null) {
+                        System.out.println("\nОшибка: книга не удалена, так как её нет в шкафу!");
+                    } else {
+                        System.out.println("\nКнига \"" + deletedBook + "\" успешно удалена!");
+                    }
                     break;
                 case "4":
                     bookshelf.clear();
+                    System.out.println("\nШкаф успешно очищен от всех книг!");
                     break;
                 case "5":
+                    System.out.println("Шкаф закрыт.");
                     return false;
                 default:
                     System.out.println("Ошибка: неверное значение меню (" + userChoice +
@@ -96,25 +102,25 @@ public class BookshelfTest {
         return true;
     }
 
-    private static String inputBookName() {
-        System.out.print("Введите название книги: ");
-        return SCAN.nextLine();
-    }
-
     private static Book addBookToShelf() {
         while (true) {
             System.out.print("\n*Введите данные книги*");
             System.out.print("\nАвтор: ");
             String bookAuthor = SCAN.nextLine();
             System.out.print("Название: ");
-            String bookName = SCAN.nextLine();
+            String title = SCAN.nextLine();
             System.out.print("Год издания: ");
-            String publicationYear = SCAN.nextLine();
+            String publishedYear = SCAN.nextLine();
             try {
-                return new Book(bookAuthor, bookName, publicationYear);
+                return new Book(bookAuthor, title, publishedYear);
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
+    }
+
+    private static String inputTitle() {
+        System.out.print("Введите название книги: ");
+        return SCAN.nextLine();
     }
 }
