a361a344ede7ef794d0961bc63209ea3b179c5f3
DenisZaika <zaikadenis2002@mail.ru>
2025-11-10 23:18:13 +0300

    Файл инициализации таблицы jaegers БД robots
    - подключение к БД robots
    - удаление уже существующей таблицы robots
    - создание таблицы jaegers с 9 полями
    - запуск файла populate.sql через команду \ir
    - запуск файла queries.sql через команду \ir

---
+++ src/com/startjava/lesson_5/init_db.sql
@@ -0,0 +1,24 @@
+\echo 'Подключение к БД robots'
+\c robots;
+
+\echo 'Удаление таблицы jaegers если она уже существует'
+DROP TABLE IF EXISTS jaegers;
+
+\echo 'Создание таблицы jaegers'
+CREATE TABLE jaegers(
+    jaeger_id  SERIAL PRIMARY KEY,
+	model_name TEXT,
+	mark       INT,
+	height     DECIMAL(4, 2),
+	weight     INT,
+	status     VARCHAR(10),
+	origin     TEXT,
+	launch     DATE,
+	kaiju_kill INT
+);
+
+\echo 'Запускаем файл populate.sql'
+\ir populate.sql;
+
+\echo 'Запускаем файл queries.sql'
+\ir queries.sql;
