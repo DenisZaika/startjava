0216fd5bd0caae05465f1b4cee00d4f84dea2b4f
DenisZaika <zaikadenis2002@mail.ru>
2025-11-11 23:18:13 +0300

    Рефакторинг кода
    - табуляция заменена на пробелы

--- src/com/startjava/lesson_5/populate.sql
+++ src/com/startjava/lesson_5/populate.sql
@@ -1,12 +1,12 @@
 \echo 'Создание 10 записей о роботах в таблице jaegers'
 INSERT INTO jaegers (model_name, mark, height, weight, status, origin, launch, kaiju_kill)
 VALUES ('Guardian Bravo', 6, 73.21, 1975, 'Abandoned', 'Japanese', '2033-12-15', 5),
-	   ('Titan Redeemer', 6, 74.07, 2256, 'Destroyed', 'Korean', '2034-01-02', 2),
-	   ('Saber Athena', 7, 76.91, 1628, 'Destroyed', 'China', '2033-12-20', 1),
-	   ('Gipsy Avenger', 6, 81.77, 2004, 'Destroyed', 'China', '2034-01-12', 1),
-	   ('Striker Eureka', 5, 76.2, 1850, 'Destroyed', 'Australia', '2019-11-02', 11),
-	   ('Bracer Phoenix', 5, 70.91, 2128, 'Destroyed', 'China', '2025-11-01', 7),
-	   ('Crimson Typhoon', 4, 76.2, 1722, 'Destroyed', 'China', '2018-08-22', 7),
-	   ('Gipsy Danger', 3, 79.25, 1980, 'Destroyed', 'USA', '2017-07-10', 9),
-	   ('Romeo Blue', 1, 77.72, 7775, 'Destroyed', 'USA', '2015-12-15', 2),
-	   ('Vulcan Specter', 6, 77.77, 2000, 'Destroyed', 'Australia', '2017-01-01', 3);
+       ('Titan Redeemer', 6, 74.07, 2256, 'Destroyed', 'Korean', '2034-01-02', 2),
+       ('Saber Athena', 7, 76.91, 1628, 'Destroyed', 'China', '2033-12-20', 1),
+       ('Gipsy Avenger', 6, 81.77, 2004, 'Destroyed', 'China', '2034-01-12', 1),
+       ('Striker Eureka', 5, 76.2, 1850, 'Destroyed', 'Australia', '2019-11-02', 11),
+       ('Bracer Phoenix', 5, 70.91, 2128, 'Destroyed', 'China', '2025-11-01', 7),
+       ('Crimson Typhoon', 4, 76.2, 1722, 'Destroyed', 'China', '2018-08-22', 7),
+       ('Gipsy Danger', 3, 79.25, 1980, 'Destroyed', 'USA', '2017-07-10', 9),
+       ('Romeo Blue', 1, 77.72, 7775, 'Destroyed', 'USA', '2015-12-15', 2),
+       ('Vulcan Specter', 6, 77.77, 2000, 'Destroyed', 'Australia', '2017-01-01', 3);
